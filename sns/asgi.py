bdb3a2a11a7187bdf6d56faeaf0a8e9ad0fb5688
wassai625 <98636488+wassai625@users.noreply.github.com>
2022-08-10 17:36:15 +0900

    Initial commit

---
+++ sns/asgi.py
@@ -0,0 +1,9 @@
+"""
+ASGI config for sns project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
